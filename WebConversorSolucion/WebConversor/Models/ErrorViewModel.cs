f290b6c3f923324edba614ca92c4bf80ef8bfb4b
Salma Idrissi Farhane- <salma.idrissi@tajamar365.com>
2024-10-16 17:30:12 +0200

    .

---
+++ WebConversorSolucion/WebConversor/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebConversor.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
