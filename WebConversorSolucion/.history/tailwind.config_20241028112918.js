603a3b17960d1432db1631c4aca82323986b681f
ZuRi-To <inigo.zuriaga@tajamar365.com>
2024-10-28 16:05:16 +0100

    cambioss

---
+++ WebConversorSolucion/.history/tailwind.config_20241028112918.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,ts}', // Escanea todos los archivos HTML y TypeScript en src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
--- WebConversorSolucion/WebConversor/Program.cs
+++ WebConversorSolucion/WebConversor/Program.cs
@@ -1,32 +1,37 @@
 using Npgsql.EntityFrameworkCore.PostgreSQL;
+
 var builder = WebApplication.CreateBuilder(args);
 
-//1- Add services to the container.
+// 1- Agregar servicios al contenedor
 builder.Services.AddControllersWithViews();
 
-/*Creamos el contexto de la base de datos*/
+/* Configuración del contexto de la base de datos */
 builder.Services.AddDbContext<DbContexto>(options =>
 {
     options.UseSqlServer(
         builder.Configuration["ConnectionStrings:CadenaConexion"]);
 });
 
-
 var app = builder.Build();
 
-//2- Configure the HTTP request pipeline.
+// 2- Configurar el pipeline de peticiones HTTP
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+
 app.UseStaticFiles();
 
 app.UseRouting();
 
 app.UseAuthorization();
-//Comentario
+
+// Ruta predeterminada para los controladores
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+// Cualquier otra ruta no encontrada se redirige al index.html de Angular
+app.MapFallbackToFile("/index.html");
 
 app.Run();
