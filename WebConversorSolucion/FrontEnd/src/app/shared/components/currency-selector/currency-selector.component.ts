1e1221982417c943e97c3b8626405920cd84496c
Iñigo Zuriaga <inigo.zuriaga@tajamar365.com>
2024-11-05 20:12:37 +0100

    button

--- WebConversorSolucion/FrontEnd/src/app/shared/components/currency-selector/currency-selector.component.ts
+++ WebConversorSolucion/FrontEnd/src/app/shared/components/currency-selector/currency-selector.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
+interface Currency {
+  symbol: string;
+  code: string;
+}
+
 @Component({
   selector: 'app-currency-selector',
   templateUrl: './currency-selector.component.html',
-  styleUrl: './currency-selector.component.css'
+  styleUrls: ['./currency-selector.component.css'],
 })
 export class CurrencySelectorComponent {
+  amount: number = 0;
+  dropdownOpen: boolean = false;
+  selectedCurrency: Currency = { symbol: '$', code: 'USD' };
 
+  currencies: Currency[] = [
+    { symbol: '$', code: 'USD' },
+    { symbol: '€', code: 'EUR' },
+    { symbol: '¥', code: 'JPY' },
+    { symbol: 'kr', code: 'DKK' },
+    { symbol: '£', code: 'GBP' },
+  ];
+
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
+
+  selectCurrency(currency: Currency) {
+    this.selectedCurrency = currency;
+    this.dropdownOpen = false;
+  }
 }
