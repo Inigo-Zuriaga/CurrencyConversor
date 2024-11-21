1d7b2e62ad1652e48b26b8f83c58da68cd16fc1a
Iñigo Zuriaga <inigo.zuriaga@tajamar365.com>
2024-11-21 20:54:35 +0100

    backend chart arreglado

--- WebConversorSolucion/WebConversor/Services/Interfaces/IApiService.cs
+++ WebConversorSolucion/WebConversor/Services/Interfaces/IApiService.cs
@@ -2,6 +2,8 @@
 
 public interface IApiService
 {
-    Task<Dictionary<string, object>> GetDataFromApiAsync(string fromCurrency, string toCurrency, decimal amount);
+    Task<Dictionary<string, object>> GetExchangeRateAsync(string fromCurrency, string toCurrency, decimal amount);
+
+    Task<Dictionary<string, object>> GetHistoricalDataAsync(string fromCurrency, string toCurrency);
 
 }
