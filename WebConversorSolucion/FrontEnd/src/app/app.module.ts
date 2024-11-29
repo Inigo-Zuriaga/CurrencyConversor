914ebde5dbe207d3be746c17a0c12e0a0bf5e5d3
Herradon <alejandro.herradon@tajamar365.com>
2024-11-29 19:40:52 +0100

    about

--- WebConversorSolucion/FrontEnd/src/app/app.module.ts
+++ WebConversorSolucion/FrontEnd/src/app/app.module.ts
@@ -21,7 +21,9 @@ import {provideAnimationsAsync} from '@angular/platform-browser/animations/async
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { ProfileComponent } from './pages/auth/profile/profile.component';
 import { AboutComponent } from './pages/about/about.component';
-
+import { CarouselModule } from 'primeng/carousel';
+import { ButtonModule } from 'primeng/button';
+import {TagModule} from 'primeng/tag';
 //En este Archivo importaremos todos los componentes que creemos y
 //los añadiremos a la lista de declarations.
 
@@ -40,6 +42,7 @@ import { AboutComponent } from './pages/about/about.component';
     HistoryComponent,
     NotFoundComponent,
     AboutComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
@@ -50,8 +53,9 @@ import { AboutComponent } from './pages/about/about.component';
     CommonModule,
     NgChartsModule,
     NgOptimizedImage,
-
-
+    CarouselModule,
+    ButtonModule,
+    TagModule
   ],
   providers: [
     {
