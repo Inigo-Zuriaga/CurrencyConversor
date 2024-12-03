22890376df2d57b5ef18f7c98e7a13e0aafe5d98
Iñigo Zuriaga <inigo.zuriaga@tajamar365.com>
2024-12-03 19:49:28 +0100

    .

--- WebConversorSolucion/FrontEnd/src/app/environments/environment.ts
+++ WebConversorSolucion/FrontEnd/src/app/environments/environment.ts
@@ -4,12 +4,10 @@
 //   apiUrl:`https://v6.exchangerate-api.com/v6/${this.apiKey}`
 // };
 
-
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:25850/api/user',
-  apiUrl2:'http://localhost:25850/api/History',
-  apiUrl3:'http://localhost:25850/api/api',
-  apiUrl4:'http://localhost:25850/api/Pdf',
+  apiUrl0: 'https://appconversor-g3aagrgqg0hrejgh.northeurope-01.azurewebsites.net/',
+  apiUrl: 'https://appconversor-g3aagrgqg0hrejgh.northeurope-01.azurewebsites.net/api/user',
+  apiUrl2: 'https://appconversor-g3aagrgqg0hrejgh.northeurope-01.azurewebsites.net/api/History',
 };
 
