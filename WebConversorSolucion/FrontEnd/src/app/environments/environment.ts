67d31ff67061df9bea4875eb06cf82c0e2110275
Iñigo Zuriaga <inigo.zuriaga@tajamar365.com>
2024-12-03 19:51:31 +0100

    Update environment.ts

--- WebConversorSolucion/FrontEnd/src/app/environments/environment.ts
+++ WebConversorSolucion/FrontEnd/src/app/environments/environment.ts
@@ -6,8 +6,8 @@
 
 export const environment = {
   production: false,
-  apiUrl0: 'https://appconversor-g3aagrgqg0hrejgh.northeurope-01.azurewebsites.net/',
-  apiUrl: 'https://appconversor-g3aagrgqg0hrejgh.northeurope-01.azurewebsites.net/api/user',
-  apiUrl2: 'https://appconversor-g3aagrgqg0hrejgh.northeurope-01.azurewebsites.net/api/History',
+  apiUrl0: 'http://localhost:25850/',
+  apiUrl: 'http://localhost:25850/api/user',
+  apiUrl2: 'http://localhost:25850/api/History',
 };
 
